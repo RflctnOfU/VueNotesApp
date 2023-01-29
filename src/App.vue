6eb2c69bb65db62e9cdf732b83d5b89064cdd4a2
Kris Marshall <rflctnofu@hotmail.com>
2023-01-29 13:38:13 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    <header><h1>Vue Notes App</h1></header>
+    <div class="container">hi yo</div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  height: 100vh;
+  width: 100vw;
+}
+
+header {
+  padding-top: 5px;
+  width: 100%;
+  height: 100px;
+  text-align: center;
+  font-size: x-large;
+  font-weight: bold;
+  background-color: darkslateblue;
+}
+</style>
