2ad509f2e800843ab057ff0d843303ca8fd2cca8
Kris Marshall <rflctnofu@hotmail.com>
2023-01-29 18:04:09 -0500

    basic textarea layout created

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
   <main>
     <header><h1>Vue Notes App</h1></header>
-    <div class="container">hi yo</div>
+    <div class="add-note-container">
+      <textarea
+        name="note"
+        id="note"
+        cols="30"
+        rows="10"
+        placeholder="New note"
+      ></textarea>
+      <button>Add note</button>
+    </div>
   </main>
 </template>
 
@@ -19,5 +28,23 @@ header {
   font-size: x-large;
   font-weight: bold;
   background-color: darkslateblue;
+}
+
+.add-note-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 5px 0;
+}
+
+.add-note-container button {
+  border-radius: 5px 5px;
+  cursor: pointer;
+  padding: 10px;
+  font-size: 17px;
+  margin-top: 5px;
+  background-color: slateblue;
+  color: gainsboro;
 }
 </style>
