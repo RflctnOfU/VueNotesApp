5fb3b2816bb3e2fe328f86d6caf45baeb3f2a045
Kris Marshall <rflctnofu@hotmail.com>
2023-02-01 16:08:30 -0500

    trying something with config file

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/VueNotesApp/" : "/",
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "https://rflctnofu.github.io//VueNotesApp/"
+      : "/",
 };
